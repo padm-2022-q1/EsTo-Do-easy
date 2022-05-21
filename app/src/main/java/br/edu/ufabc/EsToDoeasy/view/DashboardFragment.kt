c67de2973620632852c04c6e3c3886c981d44355
Jonathan Takeshi Ywahashi <jonathan.ywahashi@gmail.com>
2022-05-20 21:45:07 -0300

    wip: adds visual pie chart with MPAndroidChart

--- app/src/main/java/br/edu/ufabc/EsToDoeasy/view/DashboardFragment.kt
+++ app/src/main/java/br/edu/ufabc/EsToDoeasy/view/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package br.edu.ufabc.EsToDoeasy.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +11,13 @@ import androidx.fragment.app.activityViewModels
 import br.edu.ufabc.EsToDoeasy.R
 import br.edu.ufabc.EsToDoeasy.databinding.FragmentDashboardBinding
 import br.edu.ufabc.EsToDoeasy.viewmodel.MainViewModel
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.DefaultValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
+
 
 /**
  * Dashboard view.
@@ -34,7 +42,50 @@ class DashboardFragment : Fragment() {
         bindEvents()
     }
 
-    private fun initComponents() {}
+    private fun initComponents() {
+        binding.dashboardChartTimeSpentByGroup.apply {
+            description.isEnabled = false
+            isDrawHoleEnabled = false
+            isHighlightPerTapEnabled = false
+            setEntryLabelColor(Color.BLACK)
+            setEntryLabelTextSize(12f)
+            animateY(1400, Easing.EaseInOutQuad)
+        }
+
+        setTimeSpentByGroupData()
+    }
+
+    private fun setTimeSpentByGroupData() {
+        val entries = listOf(
+            PieEntry(1f, "PADM"),
+            PieEntry(2f, "TEST"),
+            PieEntry(10f, "Grupo 3"),
+        )
+
+        val colors = arrayListOf<Int>()
+        for (c in ColorTemplate.VORDIPLOM_COLORS) colors.add(c)
+        for (c in ColorTemplate.JOYFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.COLORFUL_COLORS) colors.add(c)
+        for (c in ColorTemplate.LIBERTY_COLORS) colors.add(c)
+        for (c in ColorTemplate.PASTEL_COLORS) colors.add(c)
+        colors.add(ColorTemplate.getHoloBlue())
+
+        val dataSet = PieDataSet(entries, "Time spent by group").apply {
+            setDrawIcons(false)
+            sliceSpace = 1f
+            this.colors = colors
+        }
+
+        val data = PieData(dataSet).apply {
+            setValueFormatter(DefaultValueFormatter(0))
+            setValueTextSize(11f)
+        }
+
+        binding.dashboardChartTimeSpentByGroup.apply {
+            this.data = data
+            invalidate()
+        }
+    }
 
     private fun bindEvents() {
         context?.let {
